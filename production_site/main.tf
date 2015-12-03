91c0b43759f2db52349557ee8763ff6d658f1898
shaunokeefe <shaun.okeefe.0@gmail.com>
2015-12-03 18:16:29 +0900

    limit access using security groups

--- production_site/main.tf
+++ production_site/main.tf
@@ -17,6 +17,43 @@ resource "aws_security_group" "company_news_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # access to tomcat HTTP port from the LB
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.company_news_lb.id}"]
+  }
+
+  # access to tomcat HTTP port from the LB
+  ingress {
+    from_port = 8443
+    to_port = 8443
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.company_news_lb.id}"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "company_news_lb" {
+  name = "company_news_lb"
+  description = "Group for a CompanyNews load balancer"
+
+  # SSH access from anywhere
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # HTTP access from anywhere
   ingress {
     from_port = 80
